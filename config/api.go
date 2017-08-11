9a910a465f4028a8edbd4b5597411adc6fa078f5
snozza <andrewcsnead@gmail.com>
2017-08-11 11:18:24 +0100

    add kinesis listener

--- config/api.go
+++ config/api.go
@@ -6,11 +6,15 @@ import (
 
 // Config provides app wide config vars
 type Config struct {
-	GOENV      string `json:"goenv"`
-	APIKey     string `json:"apikey"`
-	RabbitHost string `json:"rabbithost"`
-	QueueName  string `json:"queuename"`
-	Port       string `json:"port"`
+	GOENV              string `json:"goenv"`
+	APIKey             string `json:"apikey"`
+	RabbitHost         string `json:"rabbithost"`
+	QueueName          string `json:"queuename"`
+	Port               string `json:"port"`
+	AWSRegion          string `json:"awsregion"`
+	AWSAccessKeyID     string `json:"awsaccesskeyid"`
+	AWSSecretAccessKey string `json:"awssecretaccesskey"`
+	KinesisStream      string `json:"kinesisstream"`
 }
 
 // NewConfig returns a new Config instance bound with yaml file
---
+++ kinesis/api.go
@@ -0,0 +1,54 @@
+package kinesis
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+	"github.com/financial-times/ip-events-service/hooks"
+)
+
+// PutListen starts aws/kinesis session and puts records from input chan
+func PutListen(msgs <-chan Message, region string, stream string) {
+	creds := credentials.NewEnvCredentials()
+	_, err := creds.Get()
+	if err != nil {
+		panic(err)
+	}
+	cfg := aws.NewConfig().WithRegion(region).WithCredentials(creds)
+	s := session.New(cfg)
+	kc := kinesis.New(s)
+	streamName := aws.String(stream)
+
+	for m := range msgs {
+		fe := make([]hooks.FormattedEvent, 0)
+		if err := json.Unmarshal(m.Body, &fe); err != nil {
+			panic(err)
+		}
+		entries := make([]*kinesis.PutRecordsRequestEntry, len(fe))
+
+		for i := 0; i < len(entries); i++ {
+			d, err := json.Marshal(fe[i])
+			if err != nil {
+				panic(err)
+			}
+
+			entries[i] = &kinesis.PurRecordsRequestEntry{
+				Data:         d,
+				PartitionKey: fe[i].User.UUID,
+			}
+		}
+
+		res, err := kc.PutRecords(&kinesis.PutRecordsInput{
+			Records:    entries,
+			StreamName: streamName,
+		})
+		if err != nil {
+			log.Printf("%v\n", err)
+			panic(err)
+		}
+	}
+	log.Printf("%v\n", res)
+}
